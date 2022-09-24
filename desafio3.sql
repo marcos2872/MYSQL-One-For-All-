ea78aba7702a6f2e59c0650cc930a98b793fe86b
Marcos Souza Brito <njsu5@hotmail.com>
2022-09-24 12:11:04 -0300

    desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,8 @@
-SELECT user_name AS usuario,
-  (SELECT COUNT(user_id) FROM SpotifyClone.history_of_reproductions ) AS qt_de_musicas_ouvidas
- FROM SpotifyClone.users
+SELECT us.user_name AS usuario,
+  COUNT(hi.history_of_reproduction) AS qt_de_musicas_ouvidas,
+  ROUND((SUM(son.lasting_seconds)) / 60, 2) AS total_minutos
+FROM SpotifyClone.users AS us
+INNER JOIN SpotifyClone.history_of_reproductions AS hi ON hi.user_id = us.user_id
+INNER JOIN SpotifyClone.songs AS son ON son.song_id = hi.song_id
+GROUP BY us.user_name
+ORDER BY us.user_name;
